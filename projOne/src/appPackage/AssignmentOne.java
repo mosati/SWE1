ebffb37c942f9a0644004fe0ed8df018c4a836eb
mosati <mosatii@gmail.com>
2015-08-31 13:59:57 -0400

    Class for avg calc
    Added class and method to calculate average of array list and added try
    catch block for adding numbers to array list.

--- projOne/src/appPackage/AssignmentOne.java
+++ projOne/src/appPackage/AssignmentOne.java
@@ -1,5 +1,16 @@
 package appPackage;
 
+import java.util.ArrayList;
+
 public class AssignmentOne {
-	
+	public double ArrayAverage(ArrayList<Double> list) {
+		//Find total of objects in list and calculate average
+		double count = 0;
+		for (double i : list) {
+			count += i;
+		}
+		
+		double avg = (count / list.size());
+		return avg;
+	}
 }
--- projOne/src/appPackage/Main.java
+++ projOne/src/appPackage/Main.java
@@ -3,31 +3,29 @@ package appPackage;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws IOException {	
+		AssignmentOne findAvg = new AssignmentOne();
 		//Read in numbers from text file
 		Scanner s = new Scanner(new File("src/input.txt"));
 		ArrayList<Double> list = new ArrayList<Double>();
 		
 		//Add numbers to array list
-		while (s.hasNext()) {
-			list.add(s.nextDouble());
+		try {
+			while (s.hasNext()) {
+				list.add(s.nextDouble());
+			}
+		} catch (InputMismatchException e) {
+			System.err.println("Caught exception: " + e.getMessage());
 		}
 		
-		//Find total of objects in list and calculate average
-		double count = 0;
-		for (double i : list) {
-			count += i;
-		}
-		
-		double avg = (count / list.size());
-		System.out.println("Average of values: " + avg);
+		System.out.println("Average of values: " + findAvg.ArrayAverage(list));
 		
 		//Collections API used to find max and min values
-		System.out.println("\nLargest value: " + Collections.max(list));
+		System.out.println("Largest value: " + Collections.max(list));
 		System.out.println("Smallest value: " + Collections.min(list));
 		
 		//Collections API used to sort list in ascending order
